701b8200e0cfcf5e747add3e2a7ff0b8664f12af
Sarma Pydipally <sarma1807@users.noreply.github.com>
2022-10-25 07:05:37 -0400

    Add files via upload

--- 21_SimpleDemos/globalSettings.py
+++ 21_SimpleDemos/globalSettings.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python
 
+### import required libraries
+from cassandra import ConsistencyLevel
+
+
 ### global variables
 
 # change this to True when using this code with AstraDB : possible values : True / False
 USE_ASTRA_DB = False
+
+
+### https://docs.datastax.com/en/developer/python-driver/3.25/api/cassandra/#cassandra.ConsistencyLevel
+CASS_READ_CONSISTENCY  = ConsistencyLevel.LOCAL_QUORUM
+CASS_WRITE_CONSISTENCY = ConsistencyLevel.TWO
 
